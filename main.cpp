92fec554af0d4a5cd0c98d3f0d102614a2c62d82
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2024-10-29 22:24:03 +0000

    Ximenes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 
-string leggiComando(string ximenes)
+string leggiComando()
 {
+    string ximenes;
     do
     {
         cout << "[R] Per aumentare i fondi; [P] Per una previsione del budget; [S] Per aggiungere una spesa imprevista; [X] Per uscire" << endl;
         cin >> ximenes;
-    }while(ximenes != "R" or ximenes != "P" or ximenes != "S" or ximenes != "X");
+    }while(ximenes != "R" and ximenes != "P" and ximenes != "S" and ximenes != "X");
+    return ximenes;
 }
 
 float updateBudget(float & budget, float a)
@@ -18,27 +22,34 @@ float updateBudget(float & budget, float a)
 
 float previsioneBudget(float budget, float a)
 {
-    budget *= a;
+    budget = budget + (budget / 100)*a;
 }
 
 
 
 int main() 
 {
+    srand(time(0));
     float budget(0),a;
     string ximenes;
     do
     {
         cout << "Budget: " << budget << endl;
         ximenes = leggiComando();
+        
         if(ximenes == "R")
         {
             cin >> a;
             updateBudget(budget, a);
         }
         if(ximenes == "P"){
-            cin >> a;
+            a = -15;
+            a = a + rand() % 30;
             cout << "Budget previsto: " << previsioneBudget(budget,a) << endl;
+        }
+        if (ximenes == "S"){
+            cin >> a;
+            updateBudget(budget,(-a));
         }
     }while(ximenes != "x");
 }
